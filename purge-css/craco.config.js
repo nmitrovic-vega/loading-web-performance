3f3a3941de92e2b1f9ddba906301f74b0a19f7bf
Nikola Mitrovic <n.mitrovic@vegait.rs>
2022-11-21 17:15:49 +0100

    Remove purge postbuild.

--- purge-css/craco.config.js
+++ purge-css/craco.config.js
@@ -1,17 +1,7 @@
-// craco.config.js
-const purgecss = require('@fullhuman/postcss-purgecss');
-
 module.exports = {
   style: {
     postOptions: {
-      plugins: [
-        purgecss({
-          content: ['./src/**/*.html', './src/**/*.jsx', './src/**/*.js'],
-          keyframes: true,
-          variables: true
-        }),
-      ],
+      plugins: []
     },
   },
 };
-
